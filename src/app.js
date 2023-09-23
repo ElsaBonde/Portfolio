ef7a3c563712d4d43848c4b0c19a04be22cb94da
Elsa Bonde <elsasofiabonde@gmail.com>
2023-09-23 09:34:52 +0200

    last version with jacascript

---
+++ src/app.js
@@ -0,0 +1,6 @@
+//funktion som gör att ett tack-meddelande dykter upp på skärmen när användaren skickat sitt meddelande//
+
+function myFuntion() {
+    document.getElementById("form").innerHTML = "<br> Tack för ditt meddelande, jag svarar så snart jag kan! &#129505;"
+ }
+ 
